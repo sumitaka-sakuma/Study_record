7a024568c182e65b56f372d622fcedeb70a052ec
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-09-07 00:10:56 +0900

    routing

---
+++ app/Http/Controllers/StudiesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StudiesController extends Controller
+{
+    public function index(){
+
+        $content = 'aaa';
+
+        return view('studies.index', compact('content'));
+    }
+
+
+}
