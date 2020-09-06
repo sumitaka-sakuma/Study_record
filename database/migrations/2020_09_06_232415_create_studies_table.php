f825daaf618f214c82f4c7b90b05f3d1c5ad9504
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-09-06 23:33:44 +0900

    migration for studies

---
+++ database/migrations/2020_09_06_232415_create_studies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content', 30);
+            $table->datetime('started_time');
+            $table->datetime('ended_time');
+            $table->text('remark', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studies');
+    }
+}
